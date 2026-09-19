0320fa90691914eca1441aea1d3231f23bc6b5a0
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2026-09-20 02:04:12 +1000

    Add HackHash parser support (#60)

---
+++ gaseous-signature-parser/classes/parsers/xml/HackHash.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Xml;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using gaseous_signature_parser.models.RomSignatureObject;
+using RestEase;
+using System.Diagnostics;
+
+namespace gaseous_signature_parser.classes.parsers
+{
+    public class HackHashParser : BaseParser
+    {
+        public HackHashParser()
+        {
+
+        }
+
+        public override RomSignatureObject Parse(string XMLFile, Dictionary<string, object>? options = null)
+        {
+            // load dat file
+            XmlDocument xmlDocument = InitializeFromFile(XMLFile, out string md5Hash, out string sha1Hash);
+
+            // create object
+            RomSignatureObject signatureObject = new RomSignatureObject();
+
+            // read header
+            XmlNode xmlHeader = xmlDocument.DocumentElement.SelectSingleNode("/datafile/header");
+            ParseHeader(signatureObject, xmlHeader, "HackHash", md5Hash, sha1Hash);
+
+            // parse platform
+            string platformName = signatureObject.Name.Split('(').Last().TrimEnd(')');
+
+            // get machines
+            signatureObject.Games = new List<RomSignatureObject.Game>();
+            XmlNodeList xmlMachines = xmlDocument.DocumentElement.SelectNodes("/datafile/machine");
+            foreach (XmlNode xmlMachine in xmlMachines)
+            {
+                RomSignatureObject.Game machineObject = CreateGameObject();
+                machineObject.System = platformName;
+                machineObject.Name = xmlMachine.Attributes["name"].Value;
+                machineObject.Description = xmlMachine.Attributes["name"].Value;
+
+                if (xmlMachine.Attributes["sourcefile"] != null)
+                {
+                    AddGameFlag(machineObject, "sourcefile", xmlMachine.Attributes["sourcefile"].Value);
+                }
+
+                foreach (XmlNode childNode in xmlMachine.ChildNodes)
+                {
+                    switch (childNode.Name.ToLower())
+                    {
+                        case "description":
+                            if (childNode.InnerText.Contains("("))
+                            {
+                                string[] nameParts = childNode.InnerText.Split(new string[] { " (" }, StringSplitOptions.None);
+                                machineObject.Name = nameParts[0].Trim();
+                                if (nameParts.Length == 1)
+                                {
+                                    break;
+                                }
+                                string titleDetails = nameParts[1].TrimEnd(')');
+
+                                // split the details by comma
+                                string[] detailsParts = titleDetails.Split(new string[] { "," }, StringSplitOptions.None);
+
+                                // check if any of the details are in the country list
+                                foreach (string detail in detailsParts)
+                                {
+                                    string trimmedDetail = detail.Trim();
+                                    KeyValuePair<string, string>? countryItem = CountryLookup.ParseCountryString(trimmedDetail);
+                                    if (countryItem != null)
+                                    {
+                                        if (countryItem.HasValue && !machineObject.Country.ContainsKey(countryItem.Value.Key))
+                                        {
+                                            machineObject.Country.Add(countryItem.Value.Key, countryItem.Value.Value);
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                machineObject.Name = childNode.InnerText;
+                            }
+                            break;
+
+                        case "year":
+                            machineObject.Year = childNode.InnerText;
+                            break;
+
+                        case "manufacturer":
+                            machineObject.Publisher = childNode.InnerText;
+                            break;
+
+                        case "rom":
+                            RomSignatureObject.Game.Rom rom = ParseRomAttributes(childNode, RomSignatureObject.Game.Rom.SignatureSourceType.HackHash);
+                            machineObject.Roms.Add(rom);
+
+                            break;
+
+                        default:
+                            if (!machineObject.flags.ContainsKey(childNode.Name))
+                            {
+                                machineObject.flags.Add(childNode.Name, parser.ConvertXmlNodeToDictionary(childNode));
+                            }
+                            break;
+                    }
+                }
+                signatureObject.Games.Add(machineObject);
+            }
+
+            return signatureObject;
+        }
+
+        public override parser.SignatureParser GetXmlType(XmlDocument xml)
+        {
+            XmlNode xmlHeader = xml.DocumentElement.SelectSingleNode("/datafile/header");
+
+            if (xmlHeader == null)
+            {
+                return parser.SignatureParser.Unknown;
+            }
+
+            XmlNode xmlAuthor = xml.DocumentElement.SelectSingleNode("/datafile/header/author");
+
+            if (xmlAuthor == null || xmlAuthor.InnerText != "HackHash Community")
+            {
+                return parser.SignatureParser.Unknown;
+            }
+
+            return parser.SignatureParser.HackHash;
+        }
+    }
+}
